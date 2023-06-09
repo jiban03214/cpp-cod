c69ca20f5461a5e113e365799d2750bb9eb24394
jiban03214 <79347637+jiban03214@users.noreply.github.com>
2023-06-09 13:04:34 +0530

    Update student.cpp

--- student.cpp
+++ student.cpp
@@ -1,86 +1,79 @@
-#include <iostream>
-#include <cstring>
+#include<iostream>
 using namespace std;
-class student
-{
-protected:
-    int roll;
-    string name;
+class Student{
+	protected:
+	 string name;
+	 int Roll;
+	 public:
+	   void getdata()
+	  {
+        cout<<"\nEnter tne Name of the Student:-";
+		getline(cin,name);
+		cout<<"\nEnter the Roll:-";
+        cin>>Roll;
+	  }
+	  void putdata()
+	  {
+		cout<<"\n Name of the student is:-"<<name;
+		cout<<"\nRoll :-"<<Roll<<endl;
+	  }
 
-public:
-    void getdata()
-    {
-        cout << "Enter the name \t";
-        getline(cin, name);
-        cout << "Enter the roll \t";
-        cin >> roll;
-    }
-    void putdata()
-    {
-        cout << "Name : " << name << endl;
-        cout << "Roll : " << roll << endl;
-    }
 };
-class test : virtual public student
-{
-protected:
-    int marks1, marks2;
-
-public:
-    void getdata()
-    {
-        cout << "Enter the marks of 1st subject \t";
-        cin >> marks1;
-        cout << "Enter the marks of 2nd subject \t";
-        cin >> marks2;
-    }
-    void putdata()
-    {
-        cout << "Marks of 1st subject : " << marks1 << endl;
-        cout << "Marks of 1st subject : " << marks1 << endl;
-    }
+class Test:public virtual  Student{
+	protected:
+	 int marks1,marks2;
+	 public:
+	  void getdata()
+	  {
+		cout<<"Enter the marks1:\n";
+		cin>>marks1;
+		cout<<"Enter the marks2\n";
+		cin>>marks2;
+	  }
+	  void putdata(){
+		cout<<"Marks1 is:-"<<marks1<<endl;
+		cout<<"Marks2 is:-"<<marks2<<endl;
+	  }
 };
-class sports : virtual public student
-{
-protected:
-    int score;
+class Sports:public virtual Student{
+	protected:
+	 int score;
+	 public:
+	 void getdata()
+	 {
+		cout<<"Enter the Score:\n";
+		cin>>score;
+	 }
+	  void putdata()
+	  {
+		cout<<"score is:-"<<score<<endl;
+	  }
 
-public:
-    void getdata()
-    {
-        cout << "Enter the score \t";
-        cin >> score;
-    }
-    void putdata()
-    {
-        cout << "Sports Score : " << score << endl;
-    }
 };
-class result : public test, public sports
-{
-protected:
-    int total;
-
-public:
-    void getdata()
+class Result:public Test,public Sports{
+     protected:
+	 int Total;
+	 public:
+	 void getdata()
     {
-        student::getdata();
-        test::getdata();
-        sports::getdata();
-        total = marks1 + marks2 + score;
+        Student::getdata();
+        Test::getdata();
+        Sports::getdata();
+        Total = marks1 + marks2 + score;
     }
     void putdata()
     {
-        student::putdata();
-        test::putdata();
-        sports::putdata();
-        cout << "Total Score : " << total << endl;
+        Student::putdata();
+        Test::putdata();
+        Sports::putdata();
+        cout << "Total Score : " << Total << endl;
     }
+	 
 };
 int main()
 {
-    result r;
-    r.getdata();
-    r.putdata();
-    return 0;
+	Result k;
+	k.getdata();
+	k.putdata();
+	return(0);
 }
