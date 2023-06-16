884e1a9fe6786ead498b8240136b76f26adb252b
jiban03214 <79347637+jiban03214@users.noreply.github.com>
2023-06-16 23:06:09 +0530

    Update fille.cpp

--- fille.cpp
+++ fille.cpp
@@ -12,42 +12,41 @@ void File::create_file(string fn)
 {
 	ofstream my_file;
 	my_file.open(fn);
-	string k;
 	if(my_file.fail()!=0)
 	{
 	cout<<"File not created";
 	}
 	else
 	{
-		cout<<"Enter the Data";
+		cout<<"Enter the Data\n";
 		ch=cin.get();
-		while(ch!='\n')
+		while(ch)
 		{
 			my_file<<ch;
+			ch=cin.get();
+			if(ch=='\n')
+                break;
 		}
 		my_file.close();
-		cout<<"File create saccessfully";
-		
+		cout<<"File create successfully";
+
 	}
 }
 void File::display_file(string fn)
 {
 	ifstream my_file;
 	my_file.open(fn);
-	string x;
 	if(my_file.fail()!=0)
 	{
-		cout<<"No file found";
+		cout<<"No file found"<<endl;
 	}
 	else
 	{
-		while(1)
+        cout<<endl;
+		while(my_file.eof()==0)
 		{
-			my_file >> ch;
-			if (my_file.eof())
-				break;
-
-			cout << ch;
+			my_file.get(ch);
+			cout<<ch;
 		}
 
 	}
@@ -57,9 +56,9 @@ int main()
 {
 	string f_name;
 	File fl;
-	cout<<"Ente the filename:";
+	cout<<"Enter the filename:";
 	cin>>f_name;
 	fl.create_file(f_name);
 	fl.display_file(f_name);
-	return(0);
+
 }
