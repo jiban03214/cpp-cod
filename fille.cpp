565b0715761da00a1813986392c30d3b1846d7d5
Jiban mandal <79347637+jiban03214@users.noreply.github.com>
2023-06-22 20:55:10 +0530

    Update fille_update.cpp

--- fille.cpp
+++ fille.cpp
@@ -6,7 +6,8 @@ class File{
 	public:
 	     void create_file(string fn);
 	     void display_file(string fn);
-	     void update_file(string fn,string fn2){}
+	     string update_file(string fn);
+
 };
 void File::create_file(string fn)
 {
@@ -19,18 +20,58 @@ void File::create_file(string fn)
 	else
 	{
 		cout<<"Enter the Data\n";
-		ch=cin.get();
-		while(ch)
+		cin.get();
+		while((ch=cin.get())!='\n')
 		{
 			my_file<<ch;
-			ch=cin.get();
-			if(ch=='\n')
-                break;
 		}
 		my_file.close();
-		cout<<"File create successfully";
+		cout<<":*:File create successfully:*:";
 
 	}
+}
+string File::update_file(string fn)
+{
+    string nf;
+    cout<<"\nEnter the Name of the New File\n";
+    cin>>nf;
+    ofstream my_file;
+    my_file.open(nf);
+    ifstream my_file1;
+    my_file1.open(fn);
+    if(my_file.fail()!=0||my_file.fail()!=0)
+    {
+        cout<<"Error\n";
+        return fn;
+
+    }
+    else
+    {
+        my_file1.get(ch);
+        while(my_file1)
+        {
+            if(ch==' ')
+            {
+
+                my_file<<ch;
+                while(ch==' ')
+                {
+                    my_file1.get(ch);
+                }
+                my_file<<char(toupper(ch));
+                my_file1.get(ch);
+            }
+            else{
+                my_file<<ch;
+                my_file1.get(ch);
+            }
+        }
+    }
+    cout<<":*:New File create successfully:*:\n";
+    my_file1.close();
+    my_file.close();
+    return nf;
+
 }
 void File::display_file(string fn)
 {
@@ -43,10 +84,11 @@ void File::display_file(string fn)
 	else
 	{
         cout<<endl;
+        my_file.get(ch);
 		while(my_file.eof()==0)
 		{
-			my_file.get(ch);
 			cout<<ch;
+			my_file.get(ch);
 		}
 
 	}
@@ -56,9 +98,11 @@ int main()
 {
 	string f_name;
 	File fl;
-	cout<<"Enter the filename:";
+	cout<<"Enter the filename:\n";
 	cin>>f_name;
 	fl.create_file(f_name);
 	fl.display_file(f_name);
-
+	f_name=fl.update_file(f_name);
+	fl.display_file(f_name);
+	return 0;
 }
